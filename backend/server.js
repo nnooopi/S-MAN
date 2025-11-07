c4db4417027d1e4a9d1089cf78c9ea0b288cec6b
nnooopi <lmnnooopi@gmail.com>
2025-11-07 12:48:04 +0800

    Fix Express wildcard route error: Change app.get('*') to app.use() for catch-all handler

--- backend/server.js
+++ backend/server.js
@@ -19500,7 +19500,7 @@ if (buildPathExists) {
   
   // Catch-all handler: send back React's index.html for any non-API routes
   // This MUST be defined AFTER all API routes
-  app.get('*', (req, res) => {
+  app.use((req, res, next) => {
     // Only serve index.html for non-API routes
     if (!req.path.startsWith('/api/')) {
       res.sendFile(path.join(frontendBuildPath, 'index.html'));
